8f110c377f593557cdff8f9b9e4579363d800534
dinod <dinodtharinda2001@gmail.com>
2026-09-15 12:40:22 +0530

    Initial Commit

---
+++ step-to-step/Controllers/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  step-to-step
+//
+//  Created by Dinod Tharinda on 2026-09-15.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController{
+	override func viewDidLoad() {
+		print("Splash Loaded!")
+	}
+}
