02453bc2e63a59535800baa25c1c7d8ab7017a40
dinod <dinodtharinda2001@gmail.com>
2026-09-15 13:23:54 +0530

    Add Dashboard

---
+++ step-to-step/Controllers/DashboardViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DashboardViewController.swift
+//  step-to-step
+//
+//  Created by Dinod Tharinda on 2026-09-15.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+	
+}
---
+++ step-to-step/Others/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  step-to-step
+//
+//  Created by Dinod Tharinda on 2026-09-15.
+//
+
+import Foundation
+
+class Constants {
+	enum Storyboard : String {
+		case Dashboard = "Dashboard"
+	}
+}
