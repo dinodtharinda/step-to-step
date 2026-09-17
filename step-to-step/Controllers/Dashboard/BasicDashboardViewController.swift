6547456f315c042bcabcf22260ea56f32fd5547a
dinod <dinodtharinda2001@gmail.com>
2026-09-17 17:09:18 +0530

    Add section to basic dashboard

--- step-to-step/Controllers/Dashboard/BasicDashboardViewController.swift
+++ step-to-step/Controllers/Dashboard/BasicDashboardViewController.swift
@@ -8,9 +8,32 @@
 import UIKit
 
 class BasicDashboardViewController: DashboardViewController {
+	let rs = RecipeService()
+	
+	var localRecipes: [Recipe] = []
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		fetchData()
+	}
 
 	override var dashboardTitle: String {
 		return "Basic Dashboard"
+	}
+	
+	override var recipes: [Recipe] {
+		return localRecipes
+	}
+	
+	func fetchData(){
+		Task { [weak self] in
+			guard let `self` = self else {
+				return
+			}
+			localRecipes = await self.rs.fetchRecipes()
+			applySnapshot()
+			
+		}
 	}
 }
 
