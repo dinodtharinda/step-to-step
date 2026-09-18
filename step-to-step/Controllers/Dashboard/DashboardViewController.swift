e55838eb9aa1bc07ecf142c96faf41cba81f49c5
dinod <dinodtharinda2001@gmail.com>
2026-09-18 12:12:23 +0530

    Add Compositional Layout

--- step-to-step/Controllers/Dashboard/DashboardViewController.swift
+++ step-to-step/Controllers/Dashboard/DashboardViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-nonisolated enum PostSections: Hashable {
+nonisolated enum DashboardSections: Hashable {
 	case post
 	case recipe
 }
@@ -18,27 +18,27 @@ nonisolated enum DashboardItem: Hashable {
 }
 
 class DashboardViewController: UIViewController {
-
+	
 	@IBOutlet weak var lblTitle: UILabel!
 	@IBOutlet weak var collectionView: UICollectionView!
-
-	typealias DiffableDataSource = UICollectionViewDiffableDataSource<PostSections, DashboardItem>
-	typealias Snapshot = NSDiffableDataSourceSnapshot<PostSections, DashboardItem>
-
+	
+	typealias DiffableDataSource = UICollectionViewDiffableDataSource<DashboardSections, DashboardItem>
+	typealias Snapshot = NSDiffableDataSourceSnapshot<DashboardSections, DashboardItem>
+	
 	private var dataSource: DiffableDataSource?
-
+	
 	var dashboardTitle: String {
 		fatalError("title should initilize on subclass")
 	}
-
+	
 	var posts: [Post] {
 		return []
 	}
-
+	
 	var recipes: [Recipe] {
 		return []
 	}
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		lblTitle.text = dashboardTitle
@@ -46,16 +46,13 @@ class DashboardViewController: UIViewController {
 		setupDataSource()
 		applySnapshot()
 	}
-
+	
 	private func setupCollectionView() {
-		collectionView.delegate = self
-
-		let layout = UICollectionViewFlowLayout()
-		layout.scrollDirection = .vertical
-		layout.minimumLineSpacing = 0
-		layout.minimumInteritemSpacing = 0
+		//		collectionView.delegate = self
 		
-		collectionView.collectionViewLayout = layout
+		
+		
+		collectionView.collectionViewLayout = makeLayout()
 		
 		
 		let postNib = UINib(nibName: "PostCollectionViewCell", bundle: nil)
@@ -67,7 +64,22 @@ class DashboardViewController: UIViewController {
 		collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: SectionHeaderView.elementKind, withReuseIdentifier: SectionHeaderView.identifier)
 		
 	}
-
+	
+	
+	private func makeLayout() -> UICollectionViewCompositionalLayout {
+		let layout = UICollectionViewCompositionalLayout {[weak self] index, enviroment in
+			guard let`self` = self else { return nil }
+			let ids = self.dataSource?.snapshot().sectionIdentifiers ?? []
+			guard  ids.indices.contains(index) else { return nil }
+			
+			return getSectionFor(section: ids[index])
+			
+		}
+		
+		return layout
+		
+	}
+	
 	private func setupDataSource() {
 		dataSource = DiffableDataSource(collectionView: collectionView) { [weak self] collectionView, indexPath, postItem in
 			guard let `self` = self else {
@@ -108,29 +120,89 @@ class DashboardViewController: UIViewController {
 			
 		}
 	}
-
+	
 	func applySnapshot(animating: Bool = true) {
 		guard let ds = dataSource else {
 			return
 		}
-
+		
 		var snapshot = Snapshot()
 		snapshot.appendSections([.post, .recipe])
 		snapshot.appendItems(posts.map(DashboardItem.post),toSection: .post)
 		snapshot.appendItems(recipes.map(DashboardItem.recipe), toSection: .recipe)
 		
 		ds.apply(snapshot, animatingDifferences: false)
-
-	}
-
-}
-
-extension DashboardViewController: UICollectionViewDelegateFlowLayout {
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		return CGSize(width: collectionView.bounds.width, height: 100)
+		
 	}
 	
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-		return CGSize(width: collectionView.bounds.width, height: 100)
+}
+
+//extension DashboardViewController: UICollectionViewDelegateFlowLayout {
+//	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//		return CGSize(width: collectionView.bounds.width, height: 100)
+//	}
+//
+//	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//		return CGSize(width: collectionView.bounds.width, height: 100)
+//	}
+//}
+
+
+extension DashboardViewController {
+	
+	func getSectionFor(section: DashboardSections) -> NSCollectionLayoutSection {
+		switch section {
+		case .post:
+			return createSection(itemWidth: .fractionalWidth(1/3),
+								 itemHeight: .absolute(80),
+								 groupWidth: .fractionalWidth(1),
+								 groupHeight: .absolute(80),
+								 interItemSpace: .fixed(10),
+								 interGroupSpace: 10, sectionInsets: NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing:16)
+			)
+		case .recipe:
+			return createSection(itemWidth: .fractionalWidth(1),
+								 itemHeight: .absolute(170),
+								 groupWidth: .fractionalWidth(1),
+								 groupHeight: .absolute(170),
+								 scrollBehaviour: .groupPagingCentered,
+								 interGroupSpace: 10,
+								 sectionInsets: NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing:16),
+			)
+			
+		}
 	}
+	
+	func createSection(itemWidth: NSCollectionLayoutDimension,
+					   itemHeight: NSCollectionLayoutDimension,
+					   groupWidth: NSCollectionLayoutDimension,
+					   groupHeight: NSCollectionLayoutDimension,
+					   scrollBehaviour: UICollectionLayoutSectionOrthogonalScrollingBehavior = .none,
+					   interItemSpace:  NSCollectionLayoutSpacing = .fixed(0),
+					   interGroupSpace: Double = 0,
+					   sectionInsets: NSDirectionalEdgeInsets = .zero
+					   
+					   
+	) -> NSCollectionLayoutSection {
+		
+		let itemSize = NSCollectionLayoutSize(widthDimension: itemWidth, heightDimension: itemHeight)
+		
+		let item = NSCollectionLayoutItem(layoutSize: itemSize)
+		
+		let groupSize = NSCollectionLayoutSize(widthDimension: groupWidth, heightDimension: groupHeight)
+		
+		let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+		group.interItemSpacing = interItemSpace
+		let section = NSCollectionLayoutSection(group: group)
+		
+		section.orthogonalScrollingBehavior = scrollBehaviour
+		
+		section.interGroupSpacing = interGroupSpace
+		
+		section.contentInsets = sectionInsets
+		
+		return section
+	}
+	
+	
 }
